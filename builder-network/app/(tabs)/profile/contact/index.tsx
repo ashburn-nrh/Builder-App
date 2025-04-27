28dd4c663615f216b2dd2c726cfa564a761ffc74
ashburn noronha <109790462+ashburn-nrh@users.noreply.github.com>
2025-04-27 12:24:07 +0530

    feat:contact section

--- builder-network/app/(tabs)/profile/contact/index.tsx
+++ builder-network/app/(tabs)/profile/contact/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { router } from 'expo-router'
@@ -9,9 +9,28 @@ const index = () => {
     <SafeAreaView className='flex-1 bg-primary'>
         <View className='flex-1 px-6 py-4'>
             <View className='flex-row items-start'>
-                <Ionicons name="arrow-back" size={24} color="black" onPress={() => {router.back()}} />
+                <Ionicons name="arrow-back" size={24} color="black" onPress={() => {router.push('/(tabs)/profile')}} />
             </View>
-            <Text className='text-3xl font-bold text-gray-800 mb-8'>Contact Details</Text>
+            <Text className='text-3xl font-bold text-gray-800 mb-8 mt-3 '>Contact Details</Text>
+            <View className="p-4 rounded-lg border border-gray-400">
+            <Text className="text-xl font-bold text-gray-800 text-start">
+              Verify account to access
+            </Text>
+
+            <Text className="text-lg pr-4 text-start mt-2">
+              To access your information, please authenticate your account
+            </Text>
+
+            <View className="items-center mt-4">
+              <TouchableOpacity
+                onPress={() => {}}
+                className="bg-blue-500 px-24 py-3 rounded-lg"
+              >
+                <Text className="text-white font-medium">Invite</Text>
+              </TouchableOpacity>
+  </View>
+</View>
+
                 </View>
     </SafeAreaView>
   )
