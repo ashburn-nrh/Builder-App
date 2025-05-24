cb9f8aebf10a0173b926c55f412915a6e25404f1
ashburn noronha <109790462+ashburn-nrh@users.noreply.github.com>
2025-05-25 00:10:11 +0530

    chore:StrongestSkill update

--- builder-network/app/(tabs)/tradesperson/work-details/StrongestSkill.tsx
+++ builder-network/app/(tabs)/tradesperson/work-details/StrongestSkill.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, ScrollView, Pressable } from 'react-native';
 import { useLocalSearchParams, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import AppHeader from '@/components/AppHeader';
 
 const StrongestSkill = () => {
   const { skills } = useLocalSearchParams();
@@ -27,19 +28,34 @@ const StrongestSkill = () => {
   };
 
   return (
-    <SafeAreaView className="flex-1 bg-[#f7fdf4] p-5">
-      <Text className="text-lg text-gray-500 mb-1">Step 3/5</Text>
-      <Text className="text-3xl font-bold text-gray-800 mb-2">Select your strongest skillet</Text>
-      <Text className="text-base text-gray-700 mb-4">
-        Pass skills evaluations to unlock leads in your areas of expertise.
-      </Text>
+    <SafeAreaView className="flex-1 bg-white ">
+        <AppHeader onBackPress={() => router.push('/(tabs)/tradesperson/work-details/VerifySkillsScreen')} />
+        <View className='flex-1 bg-primary px-6 pt-5 '>
+       <View className="flex-row justify-between items-start">
+        <Text className="text-2xl font-bold">Safety & Quality</Text>
+        <View className="flex-row space-x-4 items-center">
 
-      <ScrollView className="space-y-3 mb-6">
+        </View>
+      </View>
+
+      {/* Progress Bar */}
+      <View className="h-1 bg-gray-200 rounded-full mt-4 mb-6 overflow-hidden">
+        <View className="h-1 bg-blue-500 w-3/5" />
+      </View>
+
+      {/* Step Label */}
+      <Text className="text-sm text-gray-600 font-medium mb-4">Step 3/5</Text>
+
+            {/* Content */}
+      <Text className="text-2xl font-bold mb-1">Select your Strongest Skillset</Text>
+      <Text >Pass skills evaluations to unlock leads in your areas of expertise.</Text>
+
+      <ScrollView className="space-y-3 mb-6 mt-6 ">
         {options.map((item) => (
           <Pressable
             key={item}
             onPress={() => setSelectedSkill(item)}
-            className={`flex-row items-center justify-between border px-4 py-3 rounded-lg ${
+            className={`flex-row items-center justify-between border px-4 py-3 rounded-lg mt-3 ${
               selectedSkill === item ? 'border-blue-600 bg-blue-50' : 'border-gray-300'
             }`}
           >
@@ -62,6 +78,7 @@ const StrongestSkill = () => {
       >
         <Text className="text-white text-center font-semibold">Continue</Text>
       </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
